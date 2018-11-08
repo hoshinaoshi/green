bdcbf1df2e22842bcb6828eefc46be7ce6e4113c
ayumitamai-p <ayumi.tamai@pixta.co.jp>
2018-11-08 11:03:52 +0900

    Create crawler.rb

--- crawler.rb
+++ crawler.rb
@@ -1,11 +1,16 @@
 require "selenium/webdriver"
+require "json"
+require "pry"
+
+credential_file = File.read("./credential.json")
+credential = JSON.parse(credential_file)
+
+GREEN_CLIENT_ID = credential["client_id"]
+GREEN_PASSWORD =  credential["password"]
 
 crawler = Selenium::WebDriver.for :chrome
 
 crawler.navigate.to "https://www.green-japan.com/client/login?target_url=%2Fclient%2Fsearch%2F57995"
 
-client_id_input_field = crawler.find_element(:name, "client[cd]")
-client_id_input_field.send_keys("C0002341")
-
-crawler.save_screenshot('./test.png')
-
+crawler.find_element(:name, "client[cd]").send_keys(GREEN_CLIENT_ID)
+crawler.find_element(:name, "client[password]").send_keys(GREEN_PASSWORD)
